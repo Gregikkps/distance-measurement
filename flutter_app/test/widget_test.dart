b18faa359f8becaeed2f87583fb429527b299ea8
unknown <gregikkps@gmail.com>
2023-09-29 14:47:40 +0200

    Golden init

--- flutter_app/test/widget_test.dart
+++ flutter_app/test/widget_test.dart
@@ -1,30 +1,56 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+import 'package:distance_measurement_app/main.dart';
+import 'package:distance_measurement_app/provider/app/app_state.dart';
+import 'package:distance_measurement_app/provider/menu/menu_state.dart';
+import 'package:distance_measurement_app/provider/settings/settings_state.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
+void main() {
+  testWidgets('Providers initialization', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<AppState>(create: (_) => AppState()),
+          ChangeNotifierProvider<MenuState>(create: (_) => MenuState()),
+          ChangeNotifierProvider<SettingsState>(create: (_) => SettingsState()),
+        ],
+        child: const MyApp(),
+      ),
+    );
+  });
 
-// import 'package:flutter_app/main.dart';
+  testWidgets('Golden Test Example', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<AppState>(create: (_) => AppState()),
+          ChangeNotifierProvider<MenuState>(create: (_) => MenuState()),
+          ChangeNotifierProvider<SettingsState>(create: (_) => SettingsState()),
+        ],
+        child: const MyApp(),
+      ),
+    );
 
-// void main() {
-//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//     // Build our app and trigger a frame.
-//     await tester.pumpWidget(MyApp());
+    await expectLater(
+      find.byType(MyApp),
+      matchesGoldenFile('golden_screen.png'),
+    );
+  });
+  // testWidgets('Golden Test Example', (WidgetTester tester) async {
+  //   await tester.pumpWidget(
+  //     MultiProvider(
+  //       providers: [
+  //         ChangeNotifierProvider<AppState>(create: (_) => AppState()),
+  //         ChangeNotifierProvider<MenuState>(create: (_) => MenuState()),
+  //         ChangeNotifierProvider<SettingsState>(create: (_) => SettingsState()),
+  //       ],
+  //       child: const MyApp(),
+  //     ),
+  //   );
 
-//     // Verify that our counter starts at 0.
-//     expect(find.text('0'), findsOneWidget);
-//     expect(find.text('1'), findsNothing);
-
-//     // Tap the '+' icon and trigger a frame.
-//     await tester.tap(find.byIcon(Icons.add));
-//     await tester.pump();
-
-//     // Verify that our counter has incremented.
-//     expect(find.text('0'), findsNothing);
-//     expect(find.text('1'), findsOneWidget);
-//   });
-// }
+  //   await expectLater(
+  //     find.byType(MyApp),
+  //     matchesGoldenFile('golden_example2.png'),
+  //   );
+  // });
+}
